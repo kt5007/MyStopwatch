efe70f3890035d7a895542e2f2d624de0bf06c96
kt5007 <kenjin.0546@gmail.com>
2021-07-16 06:58:44 +0900

    finished

---
+++ js/main.js
@@ -0,0 +1,73 @@
+"use strict";
+{
+  const timer = document.getElementById("timer");
+  const start = document.getElementById("start");
+  const stop = document.getElementById("stop");
+  const reset = document.getElementById("reset");
+
+  let startTime;
+  let timeoutId;
+  let elapsedTime = 0;
+
+  //秒数のカウント
+  function countUp() {
+    // console.log(Date.now() - startTime);
+    const d = new Date(Date.now() - startTime + elapsedTime);
+    const m = String(d.getMinutes()).padStart(2, "0");
+    const s = String(d.getSeconds()).padStart(2, "0");
+    const ms = String(d.getMilliseconds()).padStart(3, "0");
+    // タイマーの表示
+    timer.textContent = `${m}:${s}.${ms}`;
+
+    //10msごとに呼び込み
+    timeoutId = setTimeout(() => {
+      countUp();
+    }, 10);
+  }
+
+  //ボタンの状態
+  function setButtonStateInitial() {
+    start.classList.remove('inactive');
+    stop.classList.add('inactive');
+    reset.classList.add('inactive');
+  }
+  function setButtonStateRunning() {
+    start.classList.add('inactive');
+    stop.classList.remove('inactive');
+    reset.classList.add('inactive');
+  }
+  function setButtonStateStop() {
+    start.classList.remove('inactive');
+    stop.classList.add('inactive');
+    reset.classList.remove('inactive');
+  }
+
+  setButtonStateInitial();
+
+  //ボタンをクリックしたときの処理
+  start.addEventListener("click", () => {
+    if(start.classList.contains('inactive') === true){
+      return;
+    }
+    setButtonStateRunning();
+    startTime = Date.now();
+    countUp();
+  });
+  stop.addEventListener("click", () => {
+    if(stop.classList.contains('inactive') === true){
+      return;
+    }
+    setButtonStateStop();
+    clearTimeout(timeoutId);
+    //タイマーが走っていた時間をすべて保持
+    elapsedTime += Date.now() - startTime;
+  });
+  reset.addEventListener("click", () => {
+    if(reset.classList.contains('inactive') === true){
+      return;
+    }
+    setButtonStateInitial();
+    timer.textContent = "00:00.000";
+    elapsedTime = 0;
+  });
+}
